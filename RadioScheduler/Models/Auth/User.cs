09c024a2a460a6af58e44a23408839f07f29498e
Jonatan Ljung <jonatan.ljung@hotmail.com>
2026-01-23 12:54:17 +0100

    Add ignore on user password

--- RadioScheduler/Models/Auth/User.cs
+++ RadioScheduler/Models/Auth/User.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace RadioScheduler.Models.Auth;
 
@@ -8,6 +8,7 @@ public class User {
 	public string? FirstName { get; set; } = string.Empty;
 	public string? LastName { get; set; } = string.Empty;
 	public string Username { get; set; } = string.Empty;
+	[JsonIgnore]
 	public string Password { get; set; } = string.Empty;
 	public string? Phone { get; set; } = string.Empty;
 	public string? Email { get; set; } = string.Empty;
